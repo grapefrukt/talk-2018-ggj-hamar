17cb88cb015dc5fdf5e46689db40a5c0cf210da0
Martin Jonasson <grapefrukt@grapefrukt.com>
2018-01-24 11:23:33 +0100

    started adding notes, added timer too

--- hamar/Assets/SlideController.cs
+++ hamar/Assets/SlideController.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using TMPro;
 
 public class SlideController : MonoBehaviour {
 
 	public int index = 0;
+	float timeOffset = -1;
 
 	public BezierController bezier;
 	public PoissonController poisson;
 	public UniformPoissonDiskSampler poissonSampler;
 	public SpriteRenderer pythagoras;
 	public SpriteRenderer twofold;
-	public SpriteRenderer rymdkapsel;
+	public SpriteRenderer rymdkapsel; 
+	public TextMeshPro notes;
+	public TextMeshPro timer;
+
+	void Start(){
+		SetSlide(0);
+	}
 
 	// Update is called once per frame
 	void Update () {
+
+		var t = Time.time - timeOffset;
+		var minutes = Mathf.Floor(t / 60);
+		var seconds = Mathf.FloorToInt(t % 60);
+		timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
 		var delta = 0;
 		if (Input.GetKeyDown(KeyCode.RightArrow)) delta++;
 		if (Input.GetKeyDown(KeyCode.LeftArrow)) delta--;
@@ -27,20 +41,46 @@ public class SlideController : MonoBehaviour {
 		SetSlide(index + delta);
 	}
 
+	void StartTimer(){
+		timeOffset = Time.time;
+	}
+
 	void SetSlide(int newIndex) {
 		Debug.Log(newIndex);
 		index = newIndex;
 		DisableEverything();
 
+		var note = "";
+
 		var i = 0;
 		if (i++ == index) {
 			// empty
 		} else if (i++ == index) {
+			StartTimer();
+			note = "Today I’m going to talk about some fancy math tricks you can use in your games. Don’t worry if you’re not a math person, I hope it’s going to make sense regardless.";
+		} else if (i++ == index) {
+			note = "Game making in general and programming specifically involves a lot of abstraction, someone else has done a bunch of work you can make use of to make even cooler things.";
+		} else if (i++ == index) {
+			note = "History is full of clever people working out theorems and clever ways of solving problems that, as luck would have it, are really useful when making games thousands of years later.";
+		} else if (i++ == index) {
 			pythagoras.gameObject.SetActive(true);
+			note = "That brings us to this dude.\nPythagoras.";
+		} else if (i++ == index) {
+			pythagoras.gameObject.SetActive(true);
+			note = "He not only came up with what we today call the pythagorean theorem, which involves the length of the sides in a right angle triangle. Incredibly useful stuff for games. He did this 2.5 thousand years ago.";
+		} else if (i++ == index) {
+			pythagoras.gameObject.SetActive(true);
+			note = "He also didn’t like beans. Yes. Really. \nYou see him pictured here in a French manuscript from the 1500s going all “nope” to some fava-beans.";
+		} else if (i++ == index) {
+			note = "My name is Martin. I’m a solo indie developer and have been so for the past six years. I do a lot of things with my time, but most of it is spent programming. ";
+		} else if (i++ == index) {
+			note = "I have no proper education for this, I’m making things up as I go along. It’s been working out okay so far. ";
 		} else if (i++ == index) {
 			twofold.gameObject.SetActive(true);
+			note = "Most recently, I released a game called twofold inc. but that’s almost exactly two years ago today. Since then, I’ve been on parental leave, started three new game projects and cancelled two of them.";
 		} else if (i++ == index) {
 			rymdkapsel.gameObject.SetActive(true);
+			note = "Before that I made a game called rymdkapsel that people liked. That was my first commercial game and I was lucky enough to have it do really well.";
 		} else if (i++ == index) {
 			bezier.gameObject.SetActive(true);
 			bezier.drawSimpleLine = true;
@@ -156,6 +196,8 @@ public class SlideController : MonoBehaviour {
 		} else {
 			index = 0;
 		}
+
+		notes.text = note;
 	}
 
 	void DisableEverything() {
